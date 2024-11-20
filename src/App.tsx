f10b64cb3f4ac9531200d706b4314e1a98ac6c16
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-11-20 11:48:30 +0500

    Basic Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import KanBanBoard from '@/components/KanBanBoard'
 
 function App() {
  return(
-  <div></div>
- )
+    <KanBanBoard />
+  )
 }
 
 export default App
+
+
+
+// https://www.youtube.com/watch?v=RG-3R6Pu_Ik
---
+++ src/components/KanBanBoard.tsx
@@ -0,0 +1,9 @@
+const KanBanBoard = () => {
+  return (
+    <div className="bg-yellow-300 m-10 h-[90dvh] w-[95dvw]">
+      asdasd
+    </div>
+  )
+}
+
+export default KanBanBoard
