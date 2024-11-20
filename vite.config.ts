63684b56d2ef6286b6a424e27b1eb1cd63948edb
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-11-20 11:47:46 +0500

    Aliases in vite and ts config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@assets': '/src/assets',
+      '@utils': '/src/utils',
+    },
+  },
 })
