15f3bdc146681e566b4f385e09c5898b90ccb172
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-11-20 13:38:48 +0500

    Kanban board setup

--- src/components/KanBanBoard.tsx
+++ src/components/KanBanBoard.tsx
@@ -1,9 +1,21 @@
+import PlusIcon from "@/icons/PlusIcon"
+import { Column } from "@/types"
+import { useState } from "react"
+
 const KanBanBoard = () => {
+  const [columns, setColumns] = useState<Column[]>([])
+
   return (
-    <div className="bg-yellow-300 m-10 h-[90dvh] w-[95dvw]">
-      asdasd
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
+      <div className="m-auto">
+        <button onClick={createColumn} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
+          <PlusIcon/> Add Column
+        </button>
+      </div>
     </div>
   )
+
+  function createColumn(){}
 }
 
 export default KanBanBoard
