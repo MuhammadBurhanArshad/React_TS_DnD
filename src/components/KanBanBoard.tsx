b07a11b6f9ce07ec36e72f8c280f96ebe51bce20
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-11-25 13:23:11 +0500

    Columns Setup

--- src/components/KanBanBoard.tsx
+++ src/components/KanBanBoard.tsx
@@ -1,13 +1,19 @@
 import PlusIcon from "@/icons/PlusIcon"
 import { Column } from "@/types"
 import { useState } from "react"
+import ColumnContainer from "./ColumnContainer"
 
 const KanBanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([])
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto">
+      <div className="m-auto flex gap-4">
+        <div className="flex gap-4">
+          {columns.map(col => 
+            <ColumnContainer column={col}/>
+          )}
+        </div>
         <button onClick={createColumn} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
           <PlusIcon/> Add Column
         </button>
