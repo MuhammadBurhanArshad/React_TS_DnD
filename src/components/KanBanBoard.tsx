618b8650ec1f7129109b9b08e8d800983238dc04
Muhammad Burhan Arshad <112951994+MuhammadBurhan123@users.noreply.github.com>
2024-11-27 13:35:21 +0500

    Columns DnD

--- src/components/KanBanBoard.tsx
+++ src/components/KanBanBoard.tsx
@@ -1,23 +1,32 @@
 import PlusIcon from "@/icons/PlusIcon"
-import { Column } from "@/types"
-import { useState } from "react"
+import { Column, Id } from "@/types"
+import { useMemo, useState } from "react"
 import ColumnContainer from "./ColumnContainer"
+import { DndContext } from "@dnd-kit/core"
+import { SortableContext } from "@dnd-kit/sortable"
 
 const KanBanBoard = () => {
   const [columns, setColumns] = useState<Column[]>([])
+  const columnsId = useMemo(() => 
+    columns.map((col) => col.id), 
+  [columns])
 
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <div className="m-auto flex gap-4">
-        <div className="flex gap-4">
-          {columns.map(col => 
-            <ColumnContainer column={col}/>
-          )}
+      <DndContext>
+        <div className="m-auto flex gap-4">
+          <div className="flex gap-4">
+            <SortableContext items={columnsId}>
+              {columns.map(col => 
+                <ColumnContainer key={col.id} column={col} deleteColumn={deleteColumn}/>
+              )}
+            </SortableContext>
+          </div>
+          <button onClick={createColumn} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
+            <PlusIcon/> Add Column
+          </button>
         </div>
-        <button onClick={createColumn} className="h-[60px] w-[350px] min-w-[350px] cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor p-4 ring-rose-500 hover:ring-2 flex gap-2">
-          <PlusIcon/> Add Column
-        </button>
-      </div>
+      </DndContext>
     </div>
   )
 
@@ -28,6 +37,10 @@ const KanBanBoard = () => {
     }
     
     setColumns([...columns, columnToAdd])
+  }
+
+  function deleteColumn(columnId: Id){
+    setColumns(columns.filter(col => col.id!== columnId))
   }
 }
 
